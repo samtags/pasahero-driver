d8cca113b40348732bc6e00dc9e6bdf564702ad3
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2025-02-24 05:52:33 +0800

    chore: debug not sending find-trip request in production

--- src/services/api/findTrips.js
+++ src/services/api/findTrips.js
@@ -2,18 +2,31 @@ import axios from "axios";
 import storage from "@/src/services/storage";
 
 export default async function findTrips(location, service) {
-  console.debug("Received find trip request.", { location, service });
   const driver_id = storage.getString("user.id");
-
-  const response = await axios.get("https://driver.pasahero.app/find-trips", {
-    params: {
-      service,
-      driver_id,
-      longitude: location.longitude,
-      latitude: location.latitude,
-      heading: location.heading,
-    },
+  console.debug("Received find trip request.", {
+    location,
+    service,
+    driver_id,
   });
+
+  const response = await axios
+    .get("https://driver.pasahero.app/find-trips", {
+      params: {
+        service,
+        driver_id,
+        longitude: location.longitude,
+        latitude: location.latitude,
+        heading: location.heading,
+      },
+    })
+    .catch((err) => {
+      console.debug("Unable to find trips.", {
+        err,
+        location,
+        service,
+        driver_id,
+      });
+    });
 
   if (response.status === 200) {
     console.debug("Find trip request sent.");
