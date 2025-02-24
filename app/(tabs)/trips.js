ce2f96a3adc01375c233abe23a3df630ce0e21ee
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2025-02-25 05:54:19 +0800

    chore: remove unused lines

--- app/(tabs)/trips.js
+++ app/(tabs)/trips.js
@@ -1,12 +1,5 @@
-import { View, Text, StyleSheet } from "react-native";
 import Trips from "@/src/screens/trips";
 
 export default function Tab(props) {
   return <Trips {...props} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
